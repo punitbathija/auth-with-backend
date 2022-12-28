04a1c28100814846c31ea578590abcd658a713ba
Punit Bathija <punit644@gmail.com>
2022-12-28 13:44:01 +0530

    Seprated my logic in different files also added an dotenv file also made an basic model for user

---
+++ model/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    default: null,
+  },
+  lastName: {
+    type: String,
+    default: null,
+  },
+  country: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  token: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
