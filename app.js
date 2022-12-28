3ea3022d5602d54528499ad75962ce51e6ea8aa5
Punit Bathija <punit644@gmail.com>
2022-12-28 14:00:01 +0530

    Writting logic for registering the user also using mongoose to check for conditions

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 require("dotenv").config();
 const express = require("express");
+const User = require("./model/User");
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello From Auth System</h1>");
 });
 
+app.post("/register", (req, res) => {
+  const { firstName, lastName, country, email, password } = req.body;
+  if (!(email && password && firstName && lastName && country)) {
+    res.status(400).send("All Fields are Required");
+  }
+
+  const exsistingUser = User.findOne({ email });
+
+  if (exsistingUser) {
+    res.status(401).send("User Already Exsists");
+  }
+});
 module.exports = app;
