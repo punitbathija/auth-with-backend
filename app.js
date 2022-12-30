f798bb32a66bb65621c3bc38ee209533d4521b3d
Punit Bathija <punit644@gmail.com>
2022-12-30 13:16:19 +0530

    Completed the login route by using compare method on bcrypt

--- app.js
+++ app.js
@@ -46,8 +46,38 @@ app.post("/register", async (req, res) => {
       .then(console.log("User Registered"))
       .catch((error) => console.log(error));
 
+    User.password = undefined;
+
     //token creaton
     res.status(201).json(User);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!(email, password)) {
+      res.status(400).send("Some Field is Missing");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (User && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.SECRET_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      user.token = token;
+      user.password = undefined;
+      res.status(200).json(user);
+    }
+    res.status(400).send("email does not exsist");
   } catch (error) {
     console.log(error);
   }
